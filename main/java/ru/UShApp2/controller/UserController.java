3717b45dc81d4d80159cffbc58b93a3e48b34e4a
kutafin <sto15@mail.ru>
2017-02-21 16:32:02 +0300

    Update UserController.java

--- main/java/ru/UShApp2/controller/UserController.java
+++ main/java/ru/UShApp2/controller/UserController.java
@@ -1,9 +1,13 @@
-package ru.UShApp2.controller;
+package ru.springapp.controller;
 
-import ru.UShApp2.model.User;
-import ru.UShApp2.service.SecurityService;
-import ru.UShApp2.service.UserService;
-import ru.UShApp2.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.springapp.model.ShUrl;
+import ru.springapp.model.User;
+import ru.springapp.service.SecurityService;
+import ru.springapp.service.ShUrlService;
+import ru.springapp.service.UserService;
+import ru.springapp.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +16,28 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
+
 @Controller
 public class UserController {
 
-    @Autowired
+    private ShUrlService shUrlService;
+
     private UserService userService;
 
-    @Autowired
     private SecurityService securityService;
 
-    @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    public UserController(ShUrlService shUrlService, UserService userService, SecurityService securityService, UserValidator userValidator) {
+        this.shUrlService = shUrlService;
+        this.userService = userService;
+        this.securityService = securityService;
+        this.userValidator = userValidator;
+    }
+
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registration(Model model) {
@@ -48,21 +63,49 @@ public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
+        model.addAttribute("shUrl",new ShUrl());
+        List<ShUrl> shUrls = this.shUrlService.getUrls();
+        model.addAttribute("shUrls", shUrls);
         if (error != null) {
             model.addAttribute("error", "Неверное имя пользователя или пароль");
         }
 
         if (logout != null) {
-            model.addAttribute("message", "Выход осуществлен");
+            model.addAttribute("message", "Выход осуществлен!");
         }
 
         return "login";
     }
 
     @RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
+    public String main(Model model) {
+        model.addAttribute("shUrl",new ShUrl());
+        model.addAttribute("getUrls", this.shUrlService.getUrls());
+        return "/main";
+    }
+
+
+    @RequestMapping(value = "main/add",method = RequestMethod.POST)
+    public String createUrl(@ModelAttribute("shUrl") ShUrl shUrl){
+        if (shUrl.getIdUrl()==0){
+            this.shUrlService.createUrl(shUrl);
+        }else {
+            this.shUrlService.updateUrl(shUrl);
+        }
+
+        return "redirect:/main";
+    }
+
+    @RequestMapping(value = "remove/{idUrl}")
+    public String removeUrl(@PathVariable("idUrl") int idUrl){
+        this.shUrlService.removeUrl(idUrl);
+        return "redirect:/main";
+    }
+
+    @RequestMapping(value = "edit/{idUrl}")
+    public String editUrl(@PathVariable("idUrl") int idUrl, Model model){
+        model.addAttribute("shUrl",this.shUrlService.getUrlById(idUrl));
+        model.addAttribute("getUrls",this.shUrlService.getUrls());
         return "main";
     }
-}
-
-
+        }
