24a810cc41f81ab9b10382b3f2c30bd724607fc8
kutafin <sto15@mail.ru>
2017-03-09 18:46:46 +0300

    Update UserController.java

--- main/java/ru/UShApp2/controller/UserController.java
+++ main/java/ru/UShApp2/controller/UserController.java
@@ -1,11 +1,14 @@
 package ru.springapp.controller;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.springapp.model.ShUrl;
+import ru.springapp.model.Tag;
 import ru.springapp.model.User;
 import ru.springapp.service.SecurityService;
 import ru.springapp.service.ShUrlService;
+import ru.springapp.service.TagService;
 import ru.springapp.service.UserService;
 import ru.springapp.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,16 +29,19 @@ public class UserController {
 
     private UserService userService;
 
+    private TagService tagService;
+
     private SecurityService securityService;
 
     private UserValidator userValidator;
 
     @Autowired
-    public UserController(ShUrlService shUrlService, UserService userService, SecurityService securityService, UserValidator userValidator) {
+    public UserController(ShUrlService shUrlService, UserService userService, SecurityService securityService, UserValidator userValidator, TagService tagService) {
         this.shUrlService = shUrlService;
         this.userService = userService;
         this.securityService = securityService;
         this.userValidator = userValidator;
+        this.tagService = tagService;
     }
 
 
@@ -62,10 +68,10 @@ public class UserController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
+    public String login(@ModelAttribute("shorturl") ShUrl shUrl, Model model, String error, String logout) {
         model.addAttribute("shUrl",new ShUrl());
         List<ShUrl> shUrls = this.shUrlService.getUrls();
-        model.addAttribute("shUrls", shUrls);
+        model.addAttribute("shUrls",shUrls);
         if (error != null) {
             model.addAttribute("error", "Неверное имя пользователя или пароль");
         }
@@ -77,16 +83,27 @@ public class UserController {
         return "login";
     }
 
+    @RequestMapping(value = "/shortu{idUrl}",method = RequestMethod.GET)
+    public String shortu(@PathVariable("idUrl") Integer idUrl, Model model){
+        model.addAttribute("shUrl",this.shUrlService.getUrlById(idUrl));
+        return "shortu";
+    }
+
+
     @RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
-    public String main(Model model) {
-        model.addAttribute("shUrl",new ShUrl());
-        model.addAttribute("getUrls", this.shUrlService.getUrls());
+    public String main(@ModelAttribute("shorturl") ShUrl shUrl, Model model) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String Username = userDetails.getUsername();
+        List<ShUrl> shUrls = this.shUrlService.getUrlByUsername(Username);
+        model.addAttribute("shUrl",shUrls);
         return "/main";
     }
 
 
     @RequestMapping(value = "main/add",method = RequestMethod.POST)
     public String createUrl(@ModelAttribute("shUrl") ShUrl shUrl){
+        Tag tag = shUrl.getTag();
+        User user = shUrl.getUser();
         if (shUrl.getIdUrl()==0){
             this.shUrlService.createUrl(shUrl);
         }else {
@@ -103,9 +120,28 @@ public class UserController {
     }
 
     @RequestMapping(value = "edit/{idUrl}")
-    public String editUrl(@PathVariable("idUrl") int idUrl, Model model){
-        model.addAttribute("shUrl",this.shUrlService.getUrlById(idUrl));
-        model.addAttribute("getUrls",this.shUrlService.getUrls());
+    public String editUrl(@PathVariable("idUrl") int idUrl, Model model) {
+        model.addAttribute("shUrl", this.shUrlService.getUrlById(idUrl));
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String Username = userDetails.getUsername();
+        model.addAttribute("shUrls", this.shUrlService.getUrlByUsername(Username));
         return "main";
     }
+    @RequestMapping(value = "byTag/{tagname}",method = RequestMethod.GET)
+       public String byTag(@PathVariable("tagname") String tagname, Model model )
+        {
+            List<ShUrl> shUrls = this.shUrlService.getUrlByTagname(tagname);
+        model.addAttribute("shUrls",shUrls);
+
+return "byTag";
         }
+
+        @RequestMapping(value = "tags",method = RequestMethod.GET)
+        public String getTags(@ModelAttribute("tags") Tag tag,Model model)
+        {List<Tag> tags = this.tagService.getTags();
+            model.addAttribute("tags",tags);
+
+        return "tags";
+        }
+    }
+
